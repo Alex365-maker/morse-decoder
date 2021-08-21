56a9217691bf048b9021c063d93ca70fe3c84c32
Alex365-maker <70705449+Alex365-maker@users.noreply.github.com>
2021-08-21 23:39:30 +0300

    feat:
    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,8 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    expr.split(' ');
+    let arr = "";
+    let kol = 0;
+    let fin = '';
+    let mas = [];
+    for(let i = 0; i < expr.length; i++){
+        mas[kol]=expr[i];
+        kol++;   
+        
+    if(kol == 10){
+    kol = 0
+    arr = "";
+    for(let j = 0; j < 10; j++){
+    if(mas[j] == '1' && mas[j+1] == '0'){
+        arr += "."
+        
+    }else if(mas[j] == '1' && mas[j+1] == '1'){
+        arr += '-'
+    }
+    else if(mas[j]=='*'){
+    arr += "**";}
+    j++;
+    }
+    if(arr == "**********"){
+        fin += " ";
+    }else{fin += arr.split(' ').map(  a => a .split(' ').map(b => MORSE_TABLE[b]).join('')).join(' ')}
+    }
+    }
+    return fin;
+    }
+
 
 module.exports = {
     decode
